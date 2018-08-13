2d1e7c2f2a5cad18322cc5e10de60ad46744b534
OXAYAZA <31783@ukr.net>
2018-08-13 14:49:41 +0300

    ADD: colorful flat

---
+++ colorful-flat/js/script.js
@@ -0,0 +1,147 @@
+/* функция подключения скриптов
+ ========================================================*/
+function include(scriptUrl) {
+    document.write('<script src="' + scriptUrl + '"></script>');
+}
+
+/* isIE
+ ========================================================*/
+function isIE() {
+    var myNav = navigator.userAgent.toLowerCase();
+    return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
+};
+
+/* VIDE
+ ========================================================*/
+;(function ($) {
+    var o = $('.vide');
+    if (o.length > 0) {
+        include('js/jquery.vide.js');
+    }
+})(jQuery); 
+
+/* Mobilemenu 
+=============================================*/ 
+;(function ($) { 
+     include('js/jquery.rd-navbar.js'); 
+})(jQuery);
+
+/* Owl Carousel
+========================================================*/
+;(function ($) {
+    var o = $('.owl-carousel');
+    if (o.length > 0) {
+        include('js/owl.carousel.min.js');
+        $(document).ready(function () {
+            o.owlCarousel({
+                margin: 30,
+                smartSpeed: 450,
+                loop: true,
+                dots: true,
+                dotsEach: 1,
+                nav: false,
+                navClass: ['owl-prev fa fa-angle-left', 'owl-next fa fa-angle-right'],
+                responsive: {
+                    0: { items: 1 },
+                }
+            });
+        });
+    }
+})(jQuery);
+
+/* JQuery UI Accordion
+========================================================*/
+;(function ($) {
+    var o = $('.accordion');
+    if (o.length > 0) {
+        include('js/jquery.ui.accordion.min.js');
+
+        $(document).ready(function () {
+            o.accordion({
+                active: false,
+                header: '.accordion_header',
+                heightStyle: 'content',
+                collapsible: true,
+                beforeActivate: function (event, ui) {
+                    $(ui.oldHeader[0]).parent().removeClass("active");
+                    $(ui.newHeader[0]).parent().addClass("active");
+                }
+            });
+        });
+    }
+})(jQuery);
+
+/* Google Map
+ ========================================================*/
+;(function ($) {
+    var o = document.getElementById("google-map");
+    if (o) {
+        include('//maps.google.com/maps/api/js?sensor=false');
+        include('js/jquery.rd-google-map.js');
+
+        $(document).ready(function () {
+            var o = $('#google-map');
+            if (o.length > 0) {
+                o.googleMap({
+                    styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]
+                });
+            }
+        });
+    }
+})
+(jQuery);
+
+/* ToTop
+ ========================================================*/
+;(function ($) {
+    var o = $('html');
+    if (o.hasClass('desktop')) {
+        include('js/jquery.ui.totop.js');
+
+        $(document).ready(function () {
+            $().UItoTop({
+                easingType: 'easeOutQuart',
+                containerClass: 'toTop material-icons-arrow_drop_up'
+            });
+        });
+    }
+})(jQuery);
+
+/* Easing library (Для ToTop)
+ ========================================================*/
+include('js/jquery.easing.1.3.js');
+
+/* Parallax
+ =============================================*/
+;(function ($) {
+    include('js/jquery.rd-parallax.js');
+})(jQuery);
+
+/* FancyBox
+========================================================*/
+;(function ($) {
+    var o = $('.thumb');
+    if (o.length > 0) {
+        include('js/jquery.fancybox.js');
+        include('js/jquery.fancybox-media.js');
+        include('js/jquery.fancybox-buttons.js');
+        $(document).ready(function () {
+            o.fancybox();
+        });
+    }
+})(jQuery); 
+
+/* RD Mailform
+ =============================================*/
+;(function ($) {
+    include('js/mailform/jquery.form.min.js');
+    include('js/mailform/jquery.rd-mailform.min.js');
+})(jQuery); 
+
+/* Smooth Scroll
+ =============================================*/
+;(function ($) {
+    if ($("html").hasClass("smoothscroll")) {
+        include('js/rd-smoothscroll.js');
+    }
+})(jQuery);
