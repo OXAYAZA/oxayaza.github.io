fe4981ca912b4a943e3aebc7b606d75cf494a9a2
oxayaza <gashishmarihuanovich@gmail.com>
2020-09-29 21:33:04 +0300

    React Task

---
+++ precache-manifest.bdf73b06d930ca55e9fb28fdea24422c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eef76b3a375163c3748a63d09abe1fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fbfe2f34a11bb23a679",
+    "url": "/static/css/2.92a67de8.chunk.css"
+  },
+  {
+    "revision": "60fc0e1ad084d361dad1",
+    "url": "/static/css/main.cc82012a.chunk.css"
+  },
+  {
+    "revision": "9fbfe2f34a11bb23a679",
+    "url": "/static/js/2.795a4afe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.795a4afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60fc0e1ad084d361dad1",
+    "url": "/static/js/main.c24e1fe0.chunk.js"
+  },
+  {
+    "revision": "c8cf213d2be06fefb822",
+    "url": "/static/js/runtime-main.0eb39d29.js"
+  },
+  {
+    "revision": "eeb18405f9a1f71aae9dd82d33e322d4",
+    "url": "/static/media/slide-01.eeb18405.jpg"
+  },
+  {
+    "revision": "16fa8c34f959532dc308dab02012c72b",
+    "url": "/static/media/slide-02.16fa8c34.jpg"
+  }
+]);
