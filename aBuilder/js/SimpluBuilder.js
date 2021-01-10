d41e57974a27af159baea5b07245084415a435d5
oxayaza <gashishmarihuanovich@gmail.com>
2021-01-10 13:55:54 +0200

    Update aBuilder

--- aBuilder/js/SimpluBuilder.js
+++ aBuilder/js/SimpluBuilder.js
@@ -21,7 +21,7 @@ SimpluDebug.prototype.message = function( data ) {
 	} catch ( error ) {
 		processed = '[BAD DATA]';
 	}
-	this.item.innerText = processed;
+	this.item.innerHTML = processed;
 };
 
 
@@ -53,19 +53,107 @@ SimpluPlugin.prototype.defaults = {
 };
 
 
+// Прототип меню
+function SimpluMenu() {
+	this.createMenu();
+	this.getSelectionMode();
+	this.assignHandlers();
+
+	return this;
+}
+
+SimpluMenu.prototype.createMenu = function() {
+	this.menu = document.createElement( 'div' );
+	this.menu.className = 'simplu-menu';
+	this.menu.innerHTML =
+		`<h4 title='Frame selection mode'>Selection mode:</h4>
+		<div class='simplu-option'>
+			<input id='simplu-plugins-only' class='simplu-checkbox' type='radio' name='selection-mode' value='plugins'>
+			<label for='simplu-plugins-only' class='simplu-label'  title='Selecting only plugins'>Plugins only</label>
+		</div>
+		<div class='simplu-option'>
+			<input id='simplu-ctrl' class='simplu-checkbox' type='radio' name='selection-mode' value='ctrl'>
+			<label for='simplu-ctrl' class='simplu-label' title='Hold Ctrl for select any item'>Ctrl Mode</label>
+		</div>
+		<div class='simplu-option'>
+			<input id='simplu-free' class='simplu-checkbox' type='radio' name='selection-mode' value='free'>
+			<label for='simplu-free' class='simplu-label' title='Free selection of any item'>Free Mode</label>
+		</div>
+		<div class='simplu-option'>
+			<input id='simplu-disabled-sample' class='simplu-checkbox' type='checkbox' name='disabled-sample' disabled>
+			<label for='simplu-disabled-sample' class='simplu-label'>Disabled Sample</label>
+		</div>`;
+	this.selectionModeCheckboxes = {
+		plugins: this.menu.querySelector( '#simplu-plugins-only' ),
+		ctrl: this.menu.querySelector( '#simplu-ctrl' ),
+		gree: this.menu.querySelector( '#simplu-free' ),
+	};
+	window.builder.container.appendChild( this.menu );
+};
+
+SimpluMenu.prototype.getSelectionMode = function() {
+	switch( window.builder.frame.selectionMode ) {
+		case 'plugins':
+			this.selectionModeCheckboxes.plugins.checked = true;
+			break;
+		case 'ctrl':
+			this.selectionModeCheckboxes.ctrl.checked = true;
+			break;
+		case 'free':
+			this.selectionModeCheckboxes.gree.checked = true;
+			break;
+	}
+	console.log( '[SimpluMenu] initial selecion mode:', window.builder.frame.selectionMode );
+};
+
+SimpluMenu.prototype.assignHandlers = function() {
+	window.addEventListener( 'load', (function() {
+		for ( var key in this.selectionModeCheckboxes ) {
+			this.selectionModeCheckboxes[ key ].addEventListener( 'change', this.setSelectionMode.bind( this ) );
+		}
+	}).bind( this ));
+};
+
+SimpluMenu.prototype.setSelectionMode = function( event ) {
+	window.builder.frame.selectionMode = event.target.value;
+	console.log( '[SimpluMenu] set selecion mode:', window.builder.frame.selectionMode );
+};
+
+
 // Прототип фрейма
-function SimpluFrame( params ) {
+function SimpluFrame() {
 	// Внутренние параметры
-	this.container  = window.builder.container;
-	this.page       = window.builder.page;
-	this.buffered   = null;
-	this.target     = null;
-	this.freeSelect = true; // TODO должен зависить от режима билдера или переключаться как параметр в настройках
-	this.mode       = 'default'; // default|move
+	this.page          = window.builder.page;
+	this.buffered      = null;
+	this.target        = null;
+	this.mode          = 'default'; // default/move
+	this.selectionMode = 'ctrl';  // plugins/ctrl/free
 	this.color = {
-		default: [ '#109df7', 'white' ],
-		warning: [ '#f7e74b', 'black' ],
-		good:    [ '#7df7ac', 'white' ]
+		default: {
+			background: '#109df7',
+			color:      '#ffffff',
+			border:     '#109df7',
+			plugin:     '#ffffff',
+			tag:        '#f92672',
+			class:      '#a6e22e'
+		},
+		// TODO не используется
+		neutral: {
+			background: '#333333',
+			color:      '#ffffff',
+			border:     '#ffffff',
+			plugin:     '#109df7',
+			tag:        '#f92672',
+			class:      '#a6e22e'
+		},
+		good: {
+			background: '#00f763',
+			color:      '#313233',
+			border:     '#ffffff',
+			plugin:     '#109df7',
+			tag:        '#f92672',
+			class:      '#313233'
+		}
 	};
 
 	this.createFrame();
@@ -75,27 +163,39 @@ function SimpluFrame( params ) {
 }
 
 SimpluFrame.prototype.setColorCheme = function( name ) {
-	this.frame.style.setProperty( '--simplu-primary', this.color[ name ][ 0 ] );
-	this.frame.style.setProperty( '--simplu-secondary', this.color[ name ][ 1 ] );
+	this.frame.style.setProperty( '--simplu-background', this.color[ name ].background );
+	this.frame.style.setProperty( '--simplu-color', this.color[ name ].color );
+	this.frame.style.setProperty( '--simplu-border', this.color[ name ].border );
+	this.frame.style.setProperty( '--simplu-plugin', this.color[ name ].plugin );
+	this.frame.style.setProperty( '--simplu-tag', this.color[ name ].tag );
+	this.frame.style.setProperty( '--simplu-class', this.color[ name ].class );
 };
 
 SimpluFrame.prototype.genSelector = function( element ) {
-	var string = element.tagName.toLowerCase();
-	Array.from( element.classList ).forEach( function( item ) { string += '.'+ item; });
+	var string = `<span class='tag'>${element.tagName.toLowerCase()}</span>`;
+	Array.from( element.classList ).forEach( function( item ) { string += `<span class='class'>.${item}</span>`; });
 	return string;
 };
 
 SimpluFrame.prototype.createFrame = function() {
 	this.frame = document.createElement( 'div' );
 	this.frame.className = 'simplu-frame';
-	this.frame.innerHTML = '<div class="simplu-panel"><div class="simplu-title"></div><button class="simplu-button simplu-move fa-arrows" title="move"></button><button class="simplu-button simplu-copy fa-copy" title="copy"></button><button class="simplu-button simplu-delete fa-trash-o" title="delete"></button></div>';
+	this.frame.innerHTML = `
+		<div class='simplu-panel'>
+			<div class='simplu-title'></div>
+			<button class='simplu-button simplu-move fa-arrows' title='move'></button>
+			<button class='simplu-button simplu-up fa-level-up' title='select container'></button>
+			<button class='simplu-button simplu-copy fa-copy' title='copy'></button>
+			<button class='simplu-button simplu-delete fa-trash-o' title='delete'></button>
+		</div>`;
 	this.panel = this.frame.querySelector( '.simplu-panel' );
 	this.title = this.frame.querySelector( '.simplu-title' );
 	this.btnMove = this.frame.querySelector( '.simplu-move' );
+	this.btnUp = this.frame.querySelector( '.simplu-up' );
 	this.btnCopy = this.frame.querySelector( '.simplu-copy' );
 	this.btnDel = this.frame.querySelector( '.simplu-delete' );
-	this.setColorCheme( 'default' );
-	this.container.appendChild( this.frame );
+	this.setColorCheme( 'neutral' );
+	window.builder.container.appendChild( this.frame );
 };
 
 SimpluFrame.prototype.assignHandlers = function() {
@@ -110,16 +210,17 @@ SimpluFrame.prototype.assignHandlers = function() {
 };
 
 SimpluFrame.prototype.update = function( node, event ) {
-	var free = event && event.ctrlKey && this.freeSelect;
+	var free = ( event && event.ctrlKey && this.selectionMode === 'ctrl' ) || this.selectionMode === 'free';
+
 	if ( node && this.target !== node ) {
 		if ( free ) this.target = node;
 		else if ( node.plugin ) this.target = node;
 	}
 
-	// Скрывать рамку если Сtrl был отпущен в режиме default
-	if( event && !event.ctrlKey && this.target && !this.target.plugin && this.mode === 'default' ) {
-		this.hide();
-	}
+	// TODO Скрывать рамку если Сtrl был отпущен в режиме default
+	// if( event && !event.ctrlKey && this.target && !this.target.plugin && this.mode === 'default' ) {
+	// 	this.hide();
+	// }
 
 	if ( this.target ) {
 		if ( this.target.plugin ) console.log( '[SimpluFrame] target plugin:', this.target.plugin.params.name );
@@ -148,10 +249,10 @@ SimpluFrame.prototype.update = function( node, event ) {
 		if ( this.mode === 'default' ) {
 			// Заголовок и цветовая схема
 			if ( this.target.plugin ) {
-				this.setColorCheme( 'default' );
-				this.title.innerHTML = this.target.plugin.params.name;
+				// this.setColorCheme( 'default' );
+				this.title.innerHTML = `<span class='plugin'>${this.target.plugin.params.name}</span>`;
 			} else {
-				this.setColorCheme( 'warning' );
+				// this.setColorCheme( 'neutral' );
 				this.title.innerHTML = this.genSelector( this.target );
 			}
 
@@ -160,6 +261,13 @@ SimpluFrame.prototype.update = function( node, event ) {
 				this.btnMove.style.display = 'block';
 			} else {
 				this.btnMove.style.display = 'none';
+			}
+
+			// Кнопка перехода к родителю
+			if ( free ) {
+				this.btnUp.style.display = 'block';
+			} else {
+				this.btnUp.style.display = 'none';
 			}
 
 			// Кнопка копирования
@@ -182,8 +290,8 @@ SimpluFrame.prototype.update = function( node, event ) {
 			this.panel.style.left = event.clientX +'px';
 
 			// Цветовая схема
-			if ( this.target.plugin ) this.setColorCheme( 'default' );
-			else this.setColorCheme( 'warning' );
+			// if ( this.target.plugin ) this.setColorCheme( 'default' );
+			// else this.setColorCheme( 'neutral' );
 
 			// Заголовок
 			if ( this.buffered.plugin ) this.title.innerHTML = this.buffered.plugin.params.name;
@@ -193,6 +301,7 @@ SimpluFrame.prototype.update = function( node, event ) {
 			this.btnMove.style.display = 'none';
 			this.btnCopy.style.display = 'none';
 			this.btnDel.style.display = 'none';
+			this.btnUp.style.display = 'none';
 		}
 	}
 };
@@ -249,7 +358,7 @@ SimpluFrame.prototype.endMove = function( event ) {
 		this.panel.style.pointerEvents = 'auto';
 		this.panel.style.transition = 'none';
 
-		if ( event.target.plugin || ( event.ctrlKey && this.freeSelect && event.target ) ) {
+		if ( event.target.plugin || ( this.selectionMode === 'ctrl' && event.ctrlKey && event.target ) || ( this.selectionMode === 'free' && event.target ) ) {
 			if ( event.target.plugin ) console.log( '[SimpluFrame] moved before:', event.target.plugin );
 			else console.log( '[SimpluFrame] moved before:', this.genSelector( event.target ) );
 			this.target.parentElement.insertBefore( this.buffered, event.target );
@@ -296,11 +405,12 @@ function SimpluBuilder( options ) {
 	window.builder = this;
 	var $this = this;
 
-	this.container = document.documentElement;
-	this.page = document.querySelector( '#page' ); // TODO получать из параметра/настроек билдера/значения по умолчанию
-	this.mode = 'basic'; // TODO режимы live/basic/advanced
-	this.frame = new SimpluFrame();
-	this.debug = new SimpluDebug();
+	this.container = document.body;
+	this.page    = document.querySelector( '#page' ); // TODO получать из параметра/настроек билдера/значения по умолчанию
+	this.mode    = 'basic'; // TODO режимы live/basic/advanced
+	this.debug   = new SimpluDebug();
+	this.frame   = new SimpluFrame();
+	this.menu    = new SimpluMenu();
 	this.plugins = {};
 
 	// https://developer.mozilla.org/ru/docs/Web/API/MutationObserver
